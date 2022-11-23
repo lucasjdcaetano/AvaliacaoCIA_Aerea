d47bb0d0f81eb997079d7fcd5f6bf2fb0d8c3ed2
lucasjdcaetano <lucasjdc@outlook.com>
2022-11-23 19:56:15 -0300

    Aeronave

---
+++ Aeronave.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public abstract class Aeronave {
+    
+    private int id;
+    private String nome;
+    private int capacidade;
+    private String placa;
+
+    protected Aeronave(int id, String nome, int capacidade, String placa){
+
+    }
+}
