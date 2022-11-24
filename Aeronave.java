6f2c87a71c24c59b24714d21910ef4d37207c48e
lucasjdcaetano <lucasjdc@outlook.com>
2022-11-23 21:26:42 -0300

    Criação de superclasse e classes

--- Aeronave.java
+++ Aeronave.java
@@ -1,13 +1,39 @@
-import java.util.ArrayList;
-
-public abstract class Aeronave {
+public abstract class Aeronave implements GetId{
     
     private int id;
-    private String nome;
-    private int capacidade;
-    private String placa;
+    private String marca;
+    private String modelo;
 
-    protected Aeronave(int id, String nome, int capacidade, String placa){
-
+    protected Aeronave(int id, String marca, String modelo){
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
     }
+
+    public int getId(){
+        return id;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Id=" + id + "\n"
+            + "Marca=" + marca + "\n"
+            + "Modelo=" + modelo + "\n";
+    }
+
 }
